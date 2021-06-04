2cd7b605f478a79b0cef92ff2824e265dba1541f
Anjana0610 <84835335+Anjana0610@users.noreply.github.com>
2021-06-04 14:19:00 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,6 +5,17 @@
 
 float wallis_pi(int);
 
+float wallis_pi(int n)
+  {    float pi_value=1;
+   for(int j=1;j<=n;j++)
+   {   float a=4*j*j;
+    float b=(float)a/(a-1);
+     pi_value=pi_value*b; 
+   }
+   pi_value=2*pi_value;
+   return pi_value;
+  }
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
